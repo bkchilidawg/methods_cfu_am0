fddabad23e849de98f1c443361a9b0ac0b7f8ec2
Blaine Kennedy <blainekennedy@gmail.com>
2023-07-14 21:06:04 -0500

    Initial commit

--- built_in_methods.rb
+++ built_in_methods.rb
@@ -7,20 +7,30 @@
 # No arguments are passed; downcase has one clear job which is to lowercase all letters that exist in the String
 # The return value is "hello world"
 "Hello World".downcase
-
-
+ 
+# The .include?  method is called on the string object "Hello World"
+# One arguments are passed; downcase has one clear job which is to find out if the word "Hello" exist in the String
+# The return value is "hello world"
 "Hello World".include?("Hello")
 
-
+# The .include? method is called on the string object "Hello World"
+# One arguments are passed; .end_with? has one clear job which is to find if "Hello" ends  the String
+# The return value is "hello world"
 "Hello World".end_with?("Hello")
 
-
+# The .end_with? method is called on the string object "Hello World"
+# One arguments are passed; .end_with? has one clear job which is to find if "rld" ends the String
+# The return value is "hello world"
 "Hello World".end_with?("rld")
 
-
+# The even? method is called on the integer object 12.
+# The even? method is asking if 12 is an even number.
+# The return value is "true" because the integer is even.
 12.even?
 
-
+# The next method is called on the integer object 18.
+# The next method is asking what integer comes after 18.
+# The return value is "19"
 18.next
 
 
@@ -40,6 +50,18 @@ first_name = "Jeff"
 puts first_name.start_with?("J")
 
 
+# The length method is called on the last_name variable, which stores the string object "Kennedy".
+# The puts command prints 7 because that is how many characters long the string object is.
+last_name = "Kennedy"
+puts last_name.lenth
+
+# The include? method is called on the middle_name variable, which stores the string object "Carson".
+# The include? method returns true if the data in the middle_name variable includes the argument passed in
+# In this method call, the return value is true, because "Carson" includes "son".
+# The puts command prints the return value of the include? method (true) to the console.
+middle name = "Carson"
+puts middle_name. include? ("son")
+
 
 # SECTION 3: Calling methods on variables assigned to integers.
 # Declare 2 variables assigned to integer objects.
@@ -47,10 +69,33 @@ puts first_name.start_with?("J")
 # https://ruby-doc.org/core-3.1.0/Integer.html
 # Include comments above each method call explaining the impact and return value of that method.
 
+# The even? method is called on the age variable, which stores the integer as 29.
+# The even? method returns true if that data in the age variable is even.
+# In this method call, the puts command prints the return value of false, because 29 is odd.
+age = 29
+puts age.even?
 
+# The zero? method is called on the num variable, which stores the integer as 18.
+# The zero? method returns true if that data in the num variable integer has a zero value.
+# In this method call, the puts command prints the return value of false, because 18 does not have a zero
+num = 52
+puts num.zero?
 
 # SECTION 4: Calling methods on variables assigned to arrays.
 # Declare 2 variables assigned to arrays.
 # Call a different built-in Ruby method on each of your variables. 
 # https://ruby-doc.org/core-3.1.0/Array.html
 # Include comments above each method call explaining the impact and return value of that method.
+
+# The pop method is called on the movies variable, which stores the below array of strings.
+# The pop method returns the last item in the array.
+# In this method call, the puts command prints the return value of ScoobyDoo, because that is the last string listed in this array.
+movies = ["Frozen". "Mulan", "Hercules", "ScoobyDoo"]
+puts movies.pop
+
+# The sum method is called on the lucky_numbers variable, which stores the below array of integers.
+# The sum method adds all integers together in this array.
+# In this method call, the puts command prints the return value of 109, because 13+52+11+33=109.
+
+lucky_numbers = [13, 52, 11, 331]
+puts lucky_numbers.sum
--- self_eval.md
+++ self_eval.md
@@ -1,4 +1,7 @@
 # Self-Evaluation
 
 - Reflect on your learning and work today. What are you most proud of?
+Finding out how to do the last problem! I do think there may be a better way to do it but I got it to work!!
+
 - What is the power of using methods in our applications?
+It gives us the pwoer to use the data we are give to produce what we need it too
